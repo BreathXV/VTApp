f65e3c9a45e54d78b9ac315ecb8c155c51f9f672
Kieran <kieran@mirasaki.dev>
2024-06-06 19:43:52 +0100

    Find file path functionality and toast OS support checks

--- src/main.go
+++ src/main.go
@@ -1,20 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	beep "github.com/gen2brain/beeep"
+)
+
 // On application start, constantly checks specified folder for new files/folders,
 // then parses them to check file size
 
-type Application struct {
+type ApplicationData struct {
 	name    string
 	author  string
 	license string
-	version float32
+	version string
+}
+
+func Application() ApplicationData {
+	return ApplicationData{
+		name:    "VTApp",
+		author:  "Breath",
+		license: "Na",
+		version: "0.0.1",
+	}
+}
+
+func filePath() {
+	// Find Downloads folder
 }
 
 func main() {
-	VTApp := Application{
-		name:    "VTApp",
-		author:  "BreathXV",
-		license: "N/a",
-		version: 0.1,
+	msg := fmt.Sprintf("Application has started successfully...\n") // File path in msg
+	err := beep.Alert(Application().name, msg, "path/to/icon")
+	if beep.ErrUnsupported != nil {
+		log.Fatal("Operating system not supported\n", err)
 	}
 }
