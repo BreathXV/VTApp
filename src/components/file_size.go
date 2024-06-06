00f29ed85c0ad8fbb41aad66e670650d37f0921b
Kieran <kieran@mirasaki.dev>
2024-06-06 06:48:27 +0100

    Implemented logic for file size

--- src/components/file_size.go
+++ src/components/file_size.go
@@ -4,16 +4,20 @@ package components
 import (
 	"log"
 	"os"
+
+	ntfy "github.com/gen2brain/beeep"
 )
 
 func CheckFileSize(path string) error {
 	file, err := os.Stat(path)
 	if err != nil {
 		log.Fatalln(err)
+		return err
 	}
 
 	if file.Size() < 32000000 {
 		return nil
+		ntfy.Notify()
 		// Reg File
 	} else if file.Size() > 32000000 && file.Size() < 650000000 {
 		return nil
@@ -22,4 +26,5 @@ func CheckFileSize(path string) error {
 		return nil
 		// Return + Fatal - file to large
 	}
+	return nil
 }
