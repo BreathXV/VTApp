19e6bd5957f0e402c4e9c5f735e241527fd3b46e
Kieran <kieran@mirasaki.dev>
2024-06-07 11:00:36 +0100

    Invoke sys tray and dir window

--- src/main.go
+++ src/main.go
@@ -1,43 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-
 	cmp "github.com/breathxv/vtapp/src/components"
-	ui "github.com/ncruces/zenity"
+	"github.com/getlantern/systray"
 )
 
 // On application start, constantly checks specified folder for new files/folders,
 // then parses them to check file size
 
-func dirWindow() (path string) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		cmp.ToastAlert()
-		log.Fatal("Cannot find home directory.")
-	}
-
-	path, err = ui.SelectFile(
-		ui.Filename(homeDir),
-		ui.WindowIcon(`.\assets\RW.png`),
-		ui.Directory(),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return path
-}
-
 func main() {
-	dir, err := os.UserHomeDir()
-	if err != nil {
-		dir = dirWindow()
-		cmp.DirWatcher(dir)
-	} else {
-		path := fmt.Sprintf("%s\\Downloads", dir)
-		cmp.DirWatcher(path)
-	}
+	// TODO: Welcome screen on initial start
+	// Start directory selection
+	cmp.DirectoryParse()
+	// Start system tray
+	systray.Run(cmp.OnReady, cmp.OnExit)
 }
