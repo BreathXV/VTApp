0d4a08fa05af828d54acb11aa36d7647d66f2214
Kieran <kieran@mirasaki.dev>
2024-06-07 03:00:36 +0100

    Implemented watch loop and dir watcher

--- src/components/watcher.go
+++ src/components/watcher.go
@@ -1,5 +1,55 @@
 package components
 
+import (
+	"log"
+
+	fs "github.com/fsnotify/fsnotify"
+)
+
 // TODO: Import fsnotify
 
-func DirWatcher(path string) (file_path string, err error) {}
+func DirWatcher(path string) (file_path string, err error) {
+	w, err := fs.NewWatcher()
+	if err != nil {
+		ToastAlert()
+		log.Print("Failed to create Watcher:", err)
+		return "", err
+	}
+	defer w.Close()
+
+	go watchLoop(w)
+
+	err = w.Add(path)
+	if err != nil {
+		log.Fatalf("%q: %s", path, err)
+	}
+
+	log.Printf("ready; press ^C to exit")
+	<-make(chan struct{}) // Block forever
+	return "", nil
+}
+
+func watchLoop(w *fs.Watcher) {
+	i := 0
+	for {
+		select {
+		// Read from Errors.
+		case err, ok := <-w.Errors:
+			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
+				return
+			}
+			log.Printf("ERROR: %s", err)
+		// Read from Events.
+		case e, ok := <-w.Events:
+			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
+				return
+			}
+
+			// Just print the event nicely aligned, and keep track how many
+			// events we've seen.
+			i++
+			log.Printf("%3d", i)
+			CheckFileSize(e.Name)
+		}
+	}
+}
