81328cdcc251b8df9008b57da3c7ae0ba5923f63
Kieran <kieran@mirasaki.dev>
2024-06-08 12:06:27 +0100

    Added doc-string for directory parse and removed directory window (added into widget package)

--- src/components/directory.go
+++ src/components/directory.go
@@ -5,32 +5,21 @@ import (
 	"log"
 	"os"
 
-	ui "github.com/ncruces/zenity"
+	widget "github.com/breathxv/vtapp/src/widget"
 )
 
-func DirWindow() (path string) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		ToastAlert()
-		log.Fatal("Cannot find home directory.")
-	}
-
-	path, err = ui.SelectFile(
-		ui.Filename(homeDir),
-		ui.WindowIcon(`.\assets\VTApp.ico`),
-		ui.Directory(),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return path
-}
-
+// TODO: Add a check that the program defined path is correct.
+// Directory Parse attempts to assign the users download path.
+// It does this by first checking if their home directory exists,
+// if it does, then it will automatically attach "Downloads" onto
+// the end of the path. Otherwise, if the OS cannot locate the
+// home directory, then it call to DirWindow() which will return
+// the user's selected path. Once the path has been defined, it
+// will hand it to the DirWatcher().
 func DirectoryParse() {
 	dir, err := os.UserHomeDir()
 	if err != nil {
-		dir = DirWindow()
+		dir = widget.DirWindow()
 		log.Print("Watched directory set to path:", dir)
 		DirWatcher(dir)
 	} else {
