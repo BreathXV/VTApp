fb98fbe11e784499d7b70a8dd38d16021fdce2e0
Kieran <kieran@mirasaki.dev>
2024-06-08 12:08:24 +0100

    Added file report to display data to user from file report

---
+++ src/components/file_report.go
@@ -0,0 +1,7 @@
+package components
+
+// TODO: Build data struct and dispatch data to user
+// Find what what is received through the data struct
+// using cURL, find what data is useful and, display
+// data through Toast notification.
+// ? On click of Toast notification, can it go to the file report?
