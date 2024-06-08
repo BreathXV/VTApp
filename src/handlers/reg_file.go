10a958b3c28f78ff93d910767a465a6e5dc74118
Kieran <kieran@mirasaki.dev>
2024-06-08 12:13:14 +0100

    Added core functionality for regular file checks such as setting API key, file stats and toast alerts. Added doc-strings

--- src/handlers/reg_file.go
+++ src/handlers/reg_file.go
@@ -1,3 +1,64 @@
 package handlers
 
 // Sends files < 30MB to VirusTotal API
+
+import (
+	"flag"
+	"os"
+
+	"github.com/VirusTotal/vt-go"
+	"github.com/breathxv/vtapp/src/errors"
+	"github.com/gen2brain/beeep"
+)
+
+// TODO: Add docs for var here!
+var (
+	apikey = flag.String(
+		"apikey",
+		os.Getenv("VIRUSTOTAL_API_KEY"),
+		"VirusTotal API Key",
+	)
+)
+
+
+// Parse files takes a file or folder and hands it over to the Virus Total API 
+// for scanning. It takes one argument, path (string) and returns err (error).
+// Path should be the location of the folder/file that is to be scanned.
+// The folder cannot be a directory, it must be zipped before passing to the
+// VirusTotal API. Error is only returned in the event the function has an error.
+// The API will send an object back that is handed to the file report, which extracts
+// values from it and prompts them to the user through Toast.
+func ParseFile(path string) (err error) {
+	flag.Parse()
+
+	if *apikey == "" {
+		// Text box to set API Key
+	}
+
+	client := vt.NewClient(*apikey)
+
+	filePointer, err := os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil {
+		errors.ToastAlert()
+	}
+
+	file, err := os.Stat(path)
+	if err != nil {
+		errors.ToastAlert()
+	}
+	fileName := file.Name()
+
+	results, err := client.NewFileScanner().ScanFile(
+		filePointer,
+		nil,
+	)
+	if err != nil {
+		beeep.Alert(
+			"VTApp",
+			"Failed to send files to be scanned!",
+			"src\\assets\\VTApp.ico",
+		)
+	}
+
+	results.
+}
